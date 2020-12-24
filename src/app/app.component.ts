eb344546d09902520db7a9d49d336e9f278d708d
kalyangoud145 <kalyangoud145@gmail.com>
2020-12-24 20:08:16 +0530

    [Kalyan] added: method to redirect to a webpage on click event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,15 @@ export class AppComponent {
   title = 'HelloWorld';
   // Logo file path
   imgUrl = '../assets/BL_logo_square_jpg.jpg'
+  // Webpage url
+  url = "https://www.bridgelabz.com/"
   // For implementing one way binding
   ngOnInit(): void {
     this.title = "Hello from the bridgelabz!";
+  }
+  // Function opens the webpage in new tab
+  onClick($event) {
+    console.log($event);
+    window.open(this.url, "_blank");
   }
 }
